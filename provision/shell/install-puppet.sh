3abb7634a92b0974245e45bcd274e180d35cd6e3
Guillaume <gui.lebas@gmail.com>
2021-01-02 19:18:29 +0100

    fix(Puppet) Minor refactoring of Puppet installer

--- provision/shell/install-puppet.sh
+++ provision/shell/install-puppet.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# https://www.debian.org/releases/stretch/amd64/ch05s03.html.fr#installer-args
-export DEBIAN_FRONTEND=noninteractive
-
 PUPPET_DIR='/vagrant/provision/puppet/development'
 
 # Install Puppet Agent, Librarian Puppet (modules manager) and Puppet modules.
 if [[ ! -f /.vagrant/install-puppet ]]; then
     echo 'Vagrant: installing Puppet, Librarian Puppet, and Puppet modules'
-    apt-get -qq update && apt-get -qq install puppet librarian-puppet
+    apt-get -qq update && apt-get -qy install puppet librarian-puppet
     touch /.vagrant/install-puppet
-    cd $PUPPET_DIR && librarian-puppet install --clean
+    cd $PUPPET_DIR && librarian-puppet install
     touch /.vagrant/install-puppet-modules
     echo 'Vagrant: finished installing Puppet, Librarian Puppet, and Puppet modules'
 fi

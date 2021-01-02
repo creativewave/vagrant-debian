659721b4939b04bde7a537775949e40a7bf1549d
Guillaume <gui.lebas@gmail.com>
2021-01-02 19:32:51 +0100

    fix(NodeJS) Upgrade NodeJS version

--- provision/puppet/development/manifests/NodeJs.pp
+++ provision/puppet/development/manifests/NodeJs.pp
@@ -11,7 +11,7 @@ class cw_nodejs {
   # Add NodeJS package repository
   # Note: https requires apt-transport-https
   file { '/etc/apt/sources.list.d/nodejs.list':
-    content => 'deb http://deb.nodesource.com/node_12.x/ buster main',
+    content => 'deb http://deb.nodesource.com/node_15.x/ buster main',
     before  => Exec['update'],
   }
 

4096975c4800d92f120b9caedea67a5cbf03f019
Guillaume <gui.lebas@gmail.com>
2021-01-02 19:18:49 +0100

    fix(Puppet) Fix shared folder issue while installing Puppet modules

--- provision/shell/install-puppet.sh
+++ provision/shell/install-puppet.sh
@@ -6,6 +6,10 @@ PUPPET_DIR='/vagrant/provision/puppet/development'
 if [[ ! -f /.vagrant/install-puppet ]]; then
     echo 'Vagrant: installing Puppet, Librarian Puppet, and Puppet modules'
     apt-get -qq update && apt-get -qy install puppet librarian-puppet
+    # The line below fixes an issue related to Puppet not being able to install
+    # modules in a shared directory.
+    # No existing ticket on https://tickets.puppetlabs.com/.
+    mkdir /tmp/vagrant-puppet/librarian/ && librarian-puppet config tmp $_ --global
     touch /.vagrant/install-puppet
     cd $PUPPET_DIR && librarian-puppet install
     touch /.vagrant/install-puppet-modules
